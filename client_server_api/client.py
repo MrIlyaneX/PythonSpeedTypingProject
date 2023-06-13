10ddf56c84b1e8e0c12424b1698b3bccd2f1b64e
MrIlyaneX <mistyurin.ilya.im@gmail.com>
2023-06-13 21:46:41 +0300

    Changed to work with new UserInfo + tests

--- client_server_api/client.py
+++ client_server_api/client.py
@@ -1,5 +1,4 @@
 import requests
-
 from DB import *
 
 server_url = "http://127.0.0.1:8000"
@@ -7,7 +6,10 @@ server_url = "http://127.0.0.1:8000"
 
 def post_user_info(info: UserInfo, link: int):
     url = f"{server_url}/user/{link}"
-    response = requests.post(url, json=info.dict())
+    info_dict = info.dict()
+    info_dict['registration_date'] = info_dict['registration_date'].isoformat()
+    info_dict['last_visit'] = info_dict['last_visit'].isoformat()
+    response = requests.post(url, json=info_dict)
     return response.json()
 
 
--- client_server_api/client_runner.py
+++ client_server_api/client_runner.py
@@ -1,17 +1,30 @@
-from client import post_user_info, get_user_info
 from DB import *
+from client import post_user_info, get_user_info
 
-info = [UserInfo(name="mid", id=0), UserInfo(name="Oleg", id=1), UserInfo(name="Dom", id=2)]
+info = [
+    UserInfo(id=0, name="mid", email="mid@example.com", registration_date=datetime.now(),
+             achievements={}, last_visit=datetime.now(), days_in_row=0, max_score=0, avg_accuracy=0.0, level=0),
+    UserInfo(id=1, name="Oleg", email="oleg@example.com", registration_date=datetime.now(),
+             achievements={}, last_visit=datetime.now(), days_in_row=0, max_score=0, avg_accuracy=0.0, level=0),
+    UserInfo(id=2, name="Dom", email="dom@example.com", registration_date=datetime.now(),
+             achievements={}, last_visit=datetime.now(), days_in_row=0, max_score=0, avg_accuracy=0.0, level=0)
+]
 
-info_test = [UserInfo(name="mid", id=0), UserInfo(name="Oleg", id=1),
-             UserInfo(name="Dom", id=2), UserInfo(name="Tester", id=3)]
+info_test = [
+    UserInfo(id=0, name="mid", email="mid@example.com", registration_date=datetime.now(),
+             achievements={}, last_visit=datetime.now(), days_in_row=0, max_score=0, avg_accuracy=0.0, level=0),
+    UserInfo(id=1, name="Oleg", email="oleg@example.com", registration_date=datetime.now(),
+             achievements={}, last_visit=datetime.now(), days_in_row=0, max_score=0, avg_accuracy=0.0, level=0),
+    UserInfo(id=2, name="Dom", email="dom@example.com", registration_date=datetime.now(),
+             achievements={}, last_visit=datetime.now(), days_in_row=0, max_score=0, avg_accuracy=0.0, level=0),
+    UserInfo(id=3, name="Tester", email="tester@example.com", registration_date=datetime.now(),
+             achievements={}, last_visit=datetime.now(), days_in_row=0, max_score=0, avg_accuracy=0.0, level=0)
+]
 
-
-print("Добваляем на сервер через клиента")
+print("Putting info to api")
 for i, user in enumerate(info):
     print(post_user_info(user, i))
 
-
-print("Забираем информацию с сервера клиентом")
+print("Getting info from api")
 for i in info_test:
     print(get_user_info(i.id))
