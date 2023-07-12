355eab660fadbe8ec4e2d1704646b5c460b8ffbc
Ilia Mistiurin <i.mistiurin@innopolis.university>
2023-07-12 10:35:32 +0300

    fix of SharedData access

--- Client/__init__.py
+++ Client/__init__.py
@@ -9,25 +9,23 @@ class SharedData:
         self.header: dict | None = None
         self.user: User | None = None
 
-    def get_user(self) -> User | str:
-        if self.user is None:
-            if self.header in None:
-                user: dict = {
-                    "username": "",
-                    "email": "",
-                    "disabled": True,
-                    "hashed_password": "",
-                    "achievements": {
-                        "max_score": 0,
-                        "avg_accuracy": 0,
-                        "max_speed_accuracy": 0,
-                        "last_visit": datetime.datetime.utcnow(),
-                        "max_symbols_per_day": 0,
-                    }
+    def get_user(self) -> User:
+        if self.header is None:
+            user: dict = {
+                "username": "",
+                "email": "",
+                "disabled": True,
+                "hashed_password": "",
+                "achievements": {
+                    "max_score": 0,
+                    "avg_accuracy": 0,
+                    "max_speed_accuracy": 0,
+                    "last_visit": datetime.datetime.utcnow(),
                 }
-                return User(**user)
-            else:
-                self.user = get_user(header=self.header)
+            }
+            self.user = User(**user)
+        else:
+            self.user = get_user(header=self.header)
         return self.user
 
     def get_stats(self) -> Stats:
